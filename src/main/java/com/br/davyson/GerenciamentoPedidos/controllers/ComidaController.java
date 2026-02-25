022aa066acdc5a636a694aad6b2f79c7d3e751c4
Davyson <davyson887@gmail.com>
2026-02-25 14:16:31 -0300

    Commiting

--- src/main/java/com/br/davyson/GerenciamentoPedidos/controllers/ComidaController.java
+++ src/main/java/com/br/davyson/GerenciamentoPedidos/controllers/ComidaController.java
@@ -7,7 +7,6 @@ import com.br.davyson.GerenciamentoPedidos.services.ComidaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
